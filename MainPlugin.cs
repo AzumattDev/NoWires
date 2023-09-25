47bf1717e838618931c157c1c56efae197866234
AzumattDev <azumattgaming@gmail.com>
2023-09-25 11:11:53 -0400

    Initial commit

---
+++ MainPlugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace NoWires
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NoWiresPlugin : BaseUnityPlugin
+
+    {
+        internal const string ModName = "NoWires";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private readonly Harmony _harmony = new(ModGUID);
+        public static readonly ManualLogSource NoWiresLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private void Awake()
+        {
+            _harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Wire), nameof(Wire.Awake))]
+    static class WireAwakePatch
+    {
+        static void Postfix(Wire __instance)
+        {
+            TurnOffWire(__instance);
+        }
+
+        public static void TurnOffWire(Wire wire)
+        {
+            // Not really sure why I need reflection here, but it doesn't work without it despite being a public bool
+            bool isFinished = (bool)AccessTools.Field(typeof(Wire), "IsFinished").GetValue(wire);
+            if (isFinished)
+            {
+                wire.lineRenderer.forceRenderingOff = true;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Wire), nameof(Wire.CreateModel))]
+    static class WireCreateModelPatch
+    {
+        static void Postfix(Wire __instance)
+        {
+            WireAwakePatch.TurnOffWire(__instance);
+        }
+    }
+}
